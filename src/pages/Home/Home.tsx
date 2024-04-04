ce10be9ebcfd619a0b11608420610a12e73b9edb
Iconians <claytoncripe@gmail.com>
2024-04-04 13:20:35 -0700

    got contents on home page done will need some minor css needs done

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,20 @@
 import { Navbar } from "../../components/Navbar/Navbar";
+import styles from "./Home.module.css";
 
 export const Home = () => {
   return (
     <section className="bg-homeDesktop bg-cover h-[100vh] text-white">
       <Navbar />
-      <div className="flex justify-around content-center">
+      <div className="flex justify-around content-center mt-[250px]">
         <div className="h-[450px] w-[400px]">
           <div>
-            <p>So, you want to travel to</p>
-            <h1>Space</h1>
-          </div>
-          <div>
-            <p>
+            <p className={`text-[28px] ${styles.firstPTag}`}>
+              So, you want to travel to
+            </p>
+            <h1 className={`text-[150px] mb-6 mt-2 ${styles.h1fontFamily}`}>
+              Space
+            </h1>
+            <p className={`text-[18px] ${styles.secondPTag}`}>
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
@@ -19,8 +22,12 @@ export const Home = () => {
             </p>
           </div>
         </div>
-        <div>
-          <button>Explore</button>
+        <div className="my-auto">
+          <button
+            className={`p-[100px] bg-white text-black rounded-full text-[32px] ${styles.exploreButton}`}
+          >
+            Explore
+          </button>
         </div>
       </div>
     </section>
